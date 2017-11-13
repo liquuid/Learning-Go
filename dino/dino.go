051fb2966c803db484d5953f8b7c82197f58b4af
Fernando Henrique <liquuid@gmail.com>
2017-11-13 21:44:50 -0200

    config on json file

--- dino/dino.go
+++ dino/dino.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"dino/dynowebportal"
+	"encoding/json"
+	"log"
+	"os"
 )
 
+type configuration struct {
+	Webserver string `json:"webserver"`
+}
+
 func main() {
-	dynowebportal.RunWebPortal("localhost:8787")
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := new(configuration)
+	json.NewDecoder(file).Decode(config)
+	dynowebportal.RunWebPortal(config.Webserver)
 }
