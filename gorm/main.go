96025c9668c6d42e260fa08ce189179af68454da
Fernando Henrique <liquuid@gmail.com>
2017-11-25 18:37:23 -0200

    gorm database layer

---
+++ gorm/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "L1212")
+
+	// Update
+	db.Model(&product).Update("Price", 2000)
+
+	// Delet
+	db.Delete(&product)
+}
